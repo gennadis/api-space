c1d520f252b6b5f44a8cc10849ce021e1972f263
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-10 22:22:07 +0300

    Save images to a specified folder

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import requests
+from pathlib import Path
 
 
-def get_image(filename, url):
+def get_image(url, dirname, filename):
     response = requests.get(url)
     response.raise_for_status()
 
-    with open(filename, "wb") as file:
+    Path(dirname).mkdir(exist_ok=True)
+
+    with open(f"{dirname}/{filename}", "wb") as file:
         file.write(response.content)
 
 
 def main():
+    dirname = "images"
     filename = "hubble.jpeg"
     url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-    get_image(filename, url)
+    get_image(url, dirname, filename)
 
 
 if __name__ == "__main__":

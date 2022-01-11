02a782f0d6d9eb6f4cba9f8d7da5acfe0aa2b4d3
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-11 21:40:58 +0300

    Place constants in separate module for maintainability

---
+++ config.py
@@ -0,0 +1,15 @@
+IMAGES_DIRNAME = "images"
+
+SPACEX_BASE_URL = "https://api.spacexdata.com"
+SPACEX_ENDPOINTS = {
+    "latest": "/v4/launches/latest",
+    "all_launches": "/v4/launches",
+}
+
+# As on Jan 11 2022, latest SpaceX launch has no images,
+# hence using SpaceX CRS-20 launch ID
+CRS20_ID = "5eb87d42ffd86e000604b384"
+
+NASA_APOD_URL = "https://api.nasa.gov/planetary/apod"
+NASA_EPIC_URL = "https://api.nasa.gov/EPIC/api/natural"
+NASA_EPIC_ARCHIVE_URL = "https://api.nasa.gov/EPIC/archive/natural"

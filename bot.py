f81612bfa2dbcc88ff13211792e5a26f2cfd8023
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-11 23:17:57 +0300

    Publish photo to Telegram channel

--- bot.py
+++ bot.py
@@ -10,7 +10,10 @@ def main():
     tg_channel = os.getenv("TELEGRAM_CHANNEL")
 
     bot = telegram.Bot(token=tg_token)
-    bot.send_message(chat_id=tg_channel, text="HELLO, WORLD!")
+
+    # bot.send_message(chat_id=tg_channel, text="HELLO, WORLD!")
+    with open("images/spacex1.jpg", "rb") as photo:
+        bot.send_photo(chat_id=tg_channel, photo=photo)
 
 
 if __name__ == "__main__":

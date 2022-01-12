5881e9137e1de07abff73f80eed70ae4c37cc54c
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-12 10:51:38 +0300

    Move SpaceX photos fetching from main

---
+++ fetch_spacex.py
@@ -0,0 +1,28 @@
+import requests
+
+from utils import get_image
+
+
+def fetch_spacex_launch(
+    launch_id: str,
+    base_url: str,
+    endpoint: str,
+    dirname: str,
+) -> None:
+    """
+    Download SpaceX launch photos of launch ID given.
+    https://github.com/r-spacex/SpaceX-API
+
+    """
+
+    response = requests.get(f"{base_url}{endpoint}/{launch_id}")
+    response.raise_for_status()
+
+    original_size_links: list = response.json()["links"]["flickr"]["original"]
+
+    for count, link in enumerate(original_size_links, start=1):
+        get_image(url=link, dirname=dirname, filename=f"spacex{count}.jpg")
+
+
+if __name__ == "__main__":
+    pass

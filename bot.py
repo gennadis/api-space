f1c12490189ace53430fcec92bb6c96c620277ba
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-12 08:47:45 +0300

    Publisth photo to Telegram channel every 24 hours

--- bot.py
+++ bot.py
@@ -1,19 +1,33 @@
 import os
+from time import sleep
 
 import telegram
 from dotenv import load_dotenv
 
+import config as conf
+
+
+def send_photos(bot, dir_path: str, chat: str, delay: int):
+    """Send photos from given ditectory to Telegram chat."""
+
+    for filename in os.listdir(dir_path):
+        if filename.endswith((".jpg", ".gif", ".png")):
+            bot.send_photo(chat_id=chat, photo=open(f"{dir_path}/{filename}", "rb"))
+
+            sleep(delay)
+
 
 def main():
     load_dotenv()
-    tg_token = os.getenv("TELEGRAM_TOKEN")
-    tg_channel = os.getenv("TELEGRAM_CHANNEL")
+    bot = telegram.Bot(token=os.getenv("TELEGRAM_TOKEN"))
 
-    bot = telegram.Bot(token=tg_token)
-
-    # bot.send_message(chat_id=tg_channel, text="HELLO, WORLD!")
-    with open("images/spacex1.jpg", "rb") as photo:
-        bot.send_photo(chat_id=tg_channel, photo=photo)
+    while True:
+        send_photos(
+            bot=bot,
+            dir_path=conf.IMAGES_DIRNAME,
+            chat=os.getenv("TELEGRAM_CHANNEL"),
+            delay=conf.SLEEP_24_HOURS,
+        )
 
 
 if __name__ == "__main__":
